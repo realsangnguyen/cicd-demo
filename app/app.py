f3fe3fc60b933df5c2ddbd95504c8461b5b32ebe
user <user@localhost.localdomain>
2026-06-06 14:23:21 +0700

    Add /info endpoint

--- app/app.py
+++ app/app.py
@@ -14,5 +14,13 @@ def home():
 def health():
     return jsonify({"status": "ok"}), 200
 
+@app.route("/info")
+def info():
+    return jsonify({
+        "app": "cicd-demo",
+        "author": "Sang",
+        "environment": "production"
+    }), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
